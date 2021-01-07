50034707eddb0b750309848579aff3a380e6015e
sargisiton <108703054@g.nccu.edu.tw>
2021-01-08 03:50:03 +0800

    Displacement_Map 1.02 unfinished findsurface

--- find_surface.h
+++ find_surface.h
@@ -1,2 +1,20 @@
 #ifndef FIND_SURFACE_H
-#define FIND_
+#define FIND_SURFACE_H
+#include "vec3.h"
+#include "distance_funtion.h"
+class findsurface:public distancefuntion{
+	public:
+		findsurface();
+		~findsurface();
+		void setEyePoint(vec3);
+		void setLightPoint(vec3);
+		vec3 getEyePoint();
+		vec3 getLightPoint();
+		void find_surface();
+	private:
+		vec3 EyePoint;
+		vec3 LightPoint;
+		vec3 **DistanceMap;
+
+};
+#endif //FIND_SURFACE_H
